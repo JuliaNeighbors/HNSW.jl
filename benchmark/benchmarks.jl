4eca53cffb28eb82f0a64274c569d180339238f5
Jonas Isensee <jonas.isensee@web.de>
2018-10-17 20:22:25 +0200

    update benchmarks

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -3,14 +3,14 @@ using BenchmarkTools
 using FileIO
 
 const SUITE = BenchmarkGroup()
-SUITE["build hnsw"] = BenchmarkGroup()
+SUITE["build ANN"] = BenchmarkGroup()
 SUITE["knn"] = BenchmarkGroup()
 
 
 for dimension ∈ (1,10,100)
     for points ∈ (1000,10000)
         data = [rand(dimension) for i=1:points]
-        SUITE["build hnsw"]["dim=$dimension, points=$points"] = @benchmarkable HierarchicalNSW($data)
+        SUITE["build ANN"]["dim=$dimension, points=$points"] = @benchmarkable HierarchicalNSW($data)
         hnsw = HierarchicalNSW(data)
         for ef = (10, 100)
             for K = (1,10)
