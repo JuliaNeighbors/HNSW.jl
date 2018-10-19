bb73cc826836a15e4f193ccd337203842bea2b05
Jonas Isensee <jonas.isensee@web.de>
2018-10-19 19:48:58 +0200

    tests for low recall

--- test/compare_nearestneighbors.jl
+++ test/compare_nearestneighbors.jl
@@ -11,10 +11,10 @@ using Test
     data = [@SVector rand(Float32, dim) for n ∈ 1:num_elements]
     tree = KDTree(data)
     queries = [@SVector rand(Float32, dim) for n ∈ 1:num_queries]
-    @testset "M=$M, K=$(2M)" for M ∈ [5, 10]
-        k = 2M
-        efConstruction = 30
-        ef = 30
+    @testset "M=$M, K=1" for M ∈ [5, 10]
+        k = 1
+        efConstruction = 20
+        ef = 20
         realidxs, realdists = knn(tree, queries, k)
 
         hnsw = HierarchicalNSW(data; efConstruction=efConstruction, M=M)
@@ -25,17 +25,32 @@ using Test
                      end)
         @test ratio > 0.99
     end
-    @testset "Large K, low M=$M" for M ∈ [5]
-        efConstruction = 30
-        ef = 30
+
+    @testset "Large K, low M=$M" for M ∈ [5,10]
+        efConstruction = 100
+        ef = 50
         hnsw = HierarchicalNSW(data; efConstruction=efConstruction, M=M)
-        @testset "K=$K" for K ∈ [10, 20, 50, 100, 200, 250]
+        @testset "K=$K" for K ∈ [10,20]
             realidxs, realdists = knn(tree, queries, K)
             idxs, dists = knn_search(hnsw, queries, K, ef)
             ratio = mean(map(idxs, realidxs) do i,j
                             length(i ∩ j) / K
                          end)
-            @test ratio > 0.99
+            @test ratio > 0.9
         end
+    end
+    @testset "Low Recall Test" begin
+        k = 1
+        ef = 2
+        efConstruction = 100
+        M = 6
+        hnsw = HierarchicalNSW(data; efConstruction=efConstruction, M=M)
+        realidxs, realdists = knn(tree, queries, k)
+        idxs, dists = knn_search(hnsw, queries, k, ef)
+
+        ratio = mean(map(idxs, realidxs) do i,j
+                        length(i ∩ j) / k
+                     end)
+        @test ratio > 0.8
     end
 end
