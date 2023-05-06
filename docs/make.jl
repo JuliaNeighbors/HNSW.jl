eaf2839cbf53d95a1394446c015d19fe01ab1557
JonasIsensee <jonas.isensee@web.de>
2023-05-06 23:26:33 +0200

    Update make.jl
    remove https:// prefix

--- docs/make.jl
+++ docs/make.jl
@@ -15,4 +15,4 @@ makedocs(
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-deploydocs(repo = "https://github.com/JuliaNeighbors/HNSW.jl.git")
+deploydocs(repo = "github.com/JuliaNeighbors/HNSW.jl.git")
